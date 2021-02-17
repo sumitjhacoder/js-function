e4b990ce9e1b7fa0f3f55c3a363d6240f685bb5b
sumitjhacoder <70857738+sumitjhacoder@users.noreply.github.com>
2021-02-17 10:55:29 +0530

    Update and rename function to function.js

--- function.js
+++ function.js
@@ -3,7 +3,7 @@ var baseurl='<?php echo BASEURL ; ?>';
 var baseurl2='<?php echo BASEURL ; ?>2.6/online-test/services/';
 var loder='<div class="loading"><div class="ball"></div><b>Please wait ......</b></div>';
 */
-
+<script>
 //append select option jquery
 <!--get data class & subject --->
 var classSec2='<?php echo json_encode($classSec2Daata); ?>';
@@ -19,3 +19,26 @@ var subjectList='<?php echo json_encode($subjectListData); ?>';
         }
         $('#subject121').selectpicker('refresh');
     });
+//end
+//searc data in table
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table_id");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+  //search data in td
+    td = tr[i].getElementsByTagName("td")[5];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }        
+  }
+}
+//end
+</script>
