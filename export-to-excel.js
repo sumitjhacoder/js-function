71f23ab5560624c468e5c11dac84d65b0878043f
sumitjhacoder <70857738+sumitjhacoder@users.noreply.github.com>
2021-02-18 17:02:14 +0530

    Update export-to-excel.js

--- export-to-excel.js
+++ export-to-excel.js
@@ -1,5 +1,5 @@
 //table data export to excel
- <a href="javascript:void(0);" class="btn btn-info pull-right" style="margin-top:-20px;" title="Print Excel" onclick="download1();">
+ <a href="javascript:void(0);" class="btn btn-info pull-right" style="margin-top:-20px;" title="Print Excel" onclick="download1();">Export-to-excel</a>
  <script type="text/javascript" src="js-xlsx/xlsx.core.min.js"></script>
 <script type="text/javascript" src="js-xlsx/FileSaver.js"></script>
 <script type="text/javascript" src="js-xlsx/Export2Excel.js"></script>
